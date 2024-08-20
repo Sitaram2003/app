91cb15b9c6c2e3b6b45a8115a17a682f423e1b6e
Sitaram2003 <105721802+Sitaram2003@users.noreply.github.com>
2024-08-20 11:19:26 +0530

    Update db.js

--- vsupport-backend/config/db.js
+++ vsupport-backend/config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+const uri = "mongodb+srv://gandhamsitaramu123:J7RpN4jB90EOVRb3@cluster0.1efi0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/vsupport";
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/vsupport', {
+        await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
